9b05f8bcaa9f991554a7b1d36d0d9b34e6419da3
Thiago Moraes <thiago4x70@gmail.com>
2019-01-20 22:37:09 -0200

    Add the recipe model and output data with ngFor

--- Course-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'As the name suggests it is a test recipe. Dah!',
+      'https://source.unsplash.com/Pt_YmiYm7a4'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'As the name suggests it is a test recipe. Dah!',
+      'https://source.unsplash.com/Pt_YmiYm7a4'
+    )];
 
   constructor() { }
 
