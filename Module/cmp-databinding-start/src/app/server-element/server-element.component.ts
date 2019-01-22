8e80584065f926f8f2d7b8f9d2c0f62b5ec5e388
Thiago Moraes <thiago4x70@gmail.com>
2019-01-22 17:38:59 -0200

    Lifecycle Hooks

--- Module/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Module/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+   Component,
+   OnInit,
+   Input,
+   OnChanges,
+   SimpleChanges,
+   DoCheck,
+   AfterContentInit,
+   AfterContentChecked,
+   AfterViewInit,
+   AfterViewChecked,
+   OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+ implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+    AfterViewInit, AfterViewChecked, OnDestroy{
   //  Pass data from parent to child with input binding
   @Input('element') element: {type: string, name: string, content: string};
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input('name') name: string;
+  constructor() {
+    console.log('Constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
 }
