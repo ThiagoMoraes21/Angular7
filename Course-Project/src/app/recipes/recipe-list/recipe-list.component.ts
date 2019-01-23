9d813104f50430543520dc4ba7d86c9eb84bf8c4
Thiago Moraes <thiago4x70@gmail.com>
2019-01-23 13:55:12 -0200

    Allowing the user to Add ingredients to the shopping list

--- Course-Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course-Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,13 +12,13 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
-      'As the name suggests it is a test recipe. Dah!',
+      'As the name suggests it is a test recipe. Dah! It is a long established fact that a reader will be distracted by',
       'https://source.unsplash.com/Pt_YmiYm7a4'
     ),
     new Recipe(
-      'Test Recipe',
-      'As the name suggests it is a test recipe. Dah!',
-      'https://source.unsplash.com/Pt_YmiYm7a4'
+      'Test Recipe 2',
+      `It is a long established fact that a reader will be distracted by the readable content of a page when looking.`,
+      'https://source.unsplash.com/bpPTlXWTOvg'
     )];
 
   constructor() { }
--- Course-Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Course-Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, ElementRef, ViewChild, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    //  Take the input field values and create a new Ingredient Object
+    //  then pass this new instance of the Ingredient to the event
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+
+    //  Check if the input fields were written
+    if(ingName && ingAmount) {
+      const newIngredient = new Ingredient(ingName, ingAmount);
+      this.ingredientAdded.emit(newIngredient);
+    } else {
+      alert('Please, write the name and amount of the ingredients!');
+    }
+    
+    // Clean the input fields
+    this.nameInputRef.nativeElement.value = '';
+    this.amountInputRef.nativeElement.value = '';
   }
 
 }
