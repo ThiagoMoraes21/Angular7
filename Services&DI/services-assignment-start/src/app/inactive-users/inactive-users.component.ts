60f9fa4868dae34471f7a2f45119633b627bf7df
Thiago Moraes <thiago4x70@gmail.com>
2019-01-26 16:05:10 -0200

    Solving the services assingment

---
+++ Services&DI/services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-inactive-users',
+  templateUrl: './inactive-users.component.html',
+  styleUrls: ['./inactive-users.component.css']
+})
+export class InactiveUsersComponent {
+  @Input() users: string[];
+  // @Output() userSetToActive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersService){}
+
+  ngOnInit(){
+    this.users = this.usersService.inactiveUsers;
+  }
+
+  onSetToActive(id: number) {
+    this.usersService.onSetToActive(id);
+  }
+}
