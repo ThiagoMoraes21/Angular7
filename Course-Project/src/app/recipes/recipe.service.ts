8d10c81c59da367070c4022291191c1fc082b39d
Thiago Moraes <thiago4x70@gmail.com>
2019-01-27 19:22:48 -0200

    Adding Ingredients to Recipes

--- Course-Project/src/app/recipes/recipe.service.ts
+++ Course-Project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,25 @@ export class RecipeService {
     new Recipe(
       'Test Recipe',
       'As the name suggests it is a test recipe. Dah! It is a long established fact that a reader will be distracted by',
-      'https://source.unsplash.com/Pt_YmiYm7a4'
+      'https://source.unsplash.com/Pt_YmiYm7a4',
+      [
+        new Ingredient('Rice', 1),
+        new Ingredient('Potato', 3),
+        new Ingredient('Tomato', 2),
+        new Ingredient('Onion', 1)
+      ]
     ),
     new Recipe(
       'Test Recipe 2',
       `It is a long established fact that a reader will be distracted by the readable content of a page when looking.`,
-      'https://source.unsplash.com/bpPTlXWTOvg'
+      'https://source.unsplash.com/bpPTlXWTOvg',
+      [
+        new Ingredient('Fish', 2),
+        new Ingredient('Vegetables', 3),
+        new Ingredient('Tomato', 2),
+        new Ingredient('Onion', 1),
+        new Ingredient('Limon', 1)
+      ]
     )];
 
   constructor() { }
