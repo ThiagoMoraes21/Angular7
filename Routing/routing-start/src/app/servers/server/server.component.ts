f5febea83bce1f16455dbe8a45027d871aa1f1bf
Thiago Moraes <thiago4x70@gmail.com>
2019-02-11 17:56:50 -0200

    Protecting Routers with canActivate

--- Routing/routing-start/src/app/servers/server/server.component.ts
+++ Routing/routing-start/src/app/servers/server/server.component.ts
@@ -19,7 +19,7 @@ export class ServerComponent implements OnInit {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(+params['id']);      
+      this.server = this.serversService.getServer(+params['id']);
     });
   }
 
