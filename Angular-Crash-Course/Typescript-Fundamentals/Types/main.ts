6d1da593019e82d5310be36b490faa74e4477214
Thiago Moraes <thiago4x70@gmail.com>
2019-02-21 14:36:21 -0300

    Typescript types

---
+++ Angular-Crash-Course/Typescript-Fundamentals/Types/main.ts
@@ -0,0 +1,15 @@
+let a: number;
+let b: string;
+let c: boolean;
+let d: any;
+let e: number[];
+let f: number[] = [1, 2, 3];
+let g: any[] = [1, true, 'a'];
+
+enum Color {
+    Red = 0,
+    Green = 1,
+    Blue = 2
+}
+
+let backgroundColor = Color.Blue;
