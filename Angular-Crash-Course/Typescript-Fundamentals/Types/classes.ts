0d0a0bdc33bc8a93c18c7bd4f69938f9f320cfed
Thiago Moraes <thiago4x70@gmail.com>
2019-03-06 12:14:08 -0300

    Classes

---
+++ Angular-Crash-Course/Typescript-Fundamentals/Types/classes.ts
@@ -0,0 +1,12 @@
+class Point {
+    x: number;
+    y: number;
+    draw() {
+        console.log('X: ' + this.x, ', Y: ' + this.y);
+    }
+}
+
+let point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
