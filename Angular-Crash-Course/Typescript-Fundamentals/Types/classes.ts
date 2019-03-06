d49db35295943d559ecbe5bc78d23c287f55309d
Thiago Moraes <thiago4x70@gmail.com>
2019-03-06 12:19:50 -0300

    Constructors

--- Angular-Crash-Course/Typescript-Fundamentals/Types/classes.ts
+++ Angular-Crash-Course/Typescript-Fundamentals/Types/classes.ts
@@ -1,12 +1,17 @@
 class Point {
     x: number;
     y: number;
+
+    // the '?' makes the parameters optional
+    constructor(x?:number, y?:number) {
+        this.x = x;
+        this.y = y;
+    }
+
     draw() {
         console.log('X: ' + this.x, ', Y: ' + this.y);
     }
 }
 
 let point = new Point();
-point.x = 1;
-point.y = 2;
 point.draw();
