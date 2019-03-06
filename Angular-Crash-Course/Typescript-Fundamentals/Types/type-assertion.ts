6b0e1d8015e545287731c7301449fab1ac92f5bb
Thiago Moraes <thiago4x70@gmail.com>
2019-03-06 11:46:07 -0300

    Type Assertions

---
+++ Angular-Crash-Course/Typescript-Fundamentals/Types/type-assertion.ts
@@ -0,0 +1,19 @@
+//  By default, if you declare a variable without initialize it
+//  the type of the variable will be 'any'
+
+let message; // type any
+
+//  After initialize the variable later in the code
+//  the variable won't get access to the embedded methods of it's type
+message = 'abc'; 
+
+// The variable message, doesn't get the 'endsWith' method that any variable of type string get
+//  because if was priveously declared withou an initialization
+
+let noEndsWith = message.endsWith('c'); // nop
+
+
+// The solution to this problem is to use Type Assertion
+//  basically you declare the type of the variable in order to use it's methods
+let endsWithC = (<string>message).endsWith('c'); // most used
+let alternativeWay = (message as string).endsWith('c');
